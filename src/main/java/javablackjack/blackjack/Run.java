04c3060db361778b9a6d6a87ca2852f58f7306c3
kkssry <kkssry@naver.com>
2019-02-22 14:25:06 +0900

    step2 start!

--- src/main/java/javablackjack/blackjack/Run.java
+++ src/main/java/javablackjack/blackjack/Run.java
@@ -15,7 +15,6 @@ public class Run {
 
         Input input = new Input();
 
-
         BlackjackGame blackjackGame = new BlackjackGame(CardDeckFactory.create());
 
         blackjackGame.initUser(new User(input.getString()), new Dealer("dealer"));
@@ -36,7 +35,6 @@ public class Run {
         while (blackjackGame.isUserTurn()) {
             result = blackjackGame.userChoiceHitOrStand(input.choiceHitOrstand());
             blackjackGame.playerTurnFinish(result);
-
             OutputResult.showCard(blackjackGame.getPair());
         }
 
@@ -45,7 +43,6 @@ public class Run {
         while (blackjackGame.isDealerTurn()) {
             result = blackjackGame.dealerTurn();
             blackjackGame.playerTurnFinish(result);
-
             OutputResult.showCard(blackjackGame.getPair());
         }
 
